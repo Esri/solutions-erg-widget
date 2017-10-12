bd2cee63b0e7a344fd69c10e8ac62a2b66f09ac3
Anthony Giles <ad_giles@hotmail.com>
2017-10-12 18:15:49 +0100

    Updates 12 Oct 17 @ 18:15

--- ERG/setting/nls/strings.js
+++ ERG/setting/nls/strings.js
@@ -4,6 +4,9 @@ define({
     "layersTabLabel": "Configure Layers", // Shown as tab label for layers settings
     "symbolologyTabLabel": "Configure Symbology", // Shown as tab label for symbolology settings
     
+    "facilitiesDescription": "Facility Layers", //Shown as the facilities description
+    "demographicsDescription": "Demographics Layer", //Shown as the demographics description
+    
     
     //Settings Panel
     "settingsTitle": "Settings", // Shown as Title for Grid Settings page and label on settings buttons
@@ -54,7 +57,79 @@ define({
     "horizontal": 'Horizontal',
     "null": 'Null',
     "solid": 'Solid',
-    "vertical": 'Vertical'
+    "vertical": 'Vertical',
     
+    "addLayer": "Add Layer",
+    "layerName": "Layer",
+    "layerType": "Analysis Type",
+    "layerLabel": "Overide Layer Name",
+    "actions": "Actions",
+    "addField": "Add Field",
+    "missingLayerInWebMap": "(No operational layers found within the map)",
+    "count_checkBox": "Display feature count",
+    "area_checkBox": "Display sum of area",
+    "length_checkBox": "Display sum of length",
+    "count_label": "Count label",
+    "area_label": "Area label",
+    "length_label": "Length label",
+    "fieldTitle": "Field",
+    "actionsTitle": "Actions",
+    "layer_type_not_supported": "Layer Type Not Supported",
+    "selectFields": "Select Fields",
+    "max_records": "Maximum limit reached",
+    "invalid_string_width": "The value is too long. ",
+    "count": "COUNT",
+    "area": "AREA",
+    "length": "LENGTH",
+    
+    "demographicLayer": "Demographic Map Service",
+    "demographicLabelField": "Label Field",
+    "demographicLabel": "Description",
+    "demographicMedia": "Media",
+    
+    sourceSetting: "Search source settings",
+    instruction: "Add and configure geocode services or feature layers as search sources. These specified sources determine what is searchable within the search box.",
+    add: "Add search source",
+    addGeocoder: "Add geocoder",
+    geocoder: "Geocoder",
+    setLayerSource: "Set layer source",
+    setGeocoderURL: "Set geocoder URL",
+    searchableLayer: "Feature layer",
+    name: "Name",
+    countryCode: "Country or region code(s)",
+    countryCodeEg: "e.g. ",
+    countryCodeHint: "Leaving this value blank will search all countries and regions",
+    generalSetting: "General settings",
+    allPlaceholder: "Placeholder text for searching all: ",
+    showInfoWindowOnSelect: "Show pop-up for the found feature or location",
+    searchInCurrentMapExtent: "Only search in current map extent",
+    zoomScale: "Zoom scale",
+    locatorUrl: "Geocoder URL",
+    locatorName: "Geocoder name",
+    locatorExample: "Example",
+    locatorWarning: "This version of geocoding service is not supported. The widget supports geocoding service 10.1 and above.",
+    locatorTips: "Suggestions are not available because the geocoding service doesn't support suggest capability.",
+    layerSource: "Layer source",
+    searchLayerTips: "Suggestions are not available because the feature service doesn't support pagination capability.",
+    placeholder: "Placeholder text",
+    searchFields: "Search fields",
+    displayField: "Display field",
+    exactMatch: "Exact match",
+    maxSuggestions: "Maximum suggestions",
+    maxResults: "Maximum results",
+    enableLocalSearch: "Enable local search",
+    minScale: "Min scale",
+    minScaleHint: "When the map scale is larger than this scale, local search will be applied",
+    radius: "Radius",
+    radiusHint: "Specifies the radius of an area around current map center that is used to boost the rank of geocoding candidates so that candidates closest to the location are returned first",
+    meters: "Meters",
+    setSearchFields: "Set search fields",
+    set: "Set",
+    fieldSearchable: "searchable",
+    fieldName: "Name",
+    fieldAlias: "Alias",
+    ok: "OK",
+    cancel: "Cancel",
+    invalidUrlTip: "The URL ${URL} is invalid or inaccessible."
     })
 });
